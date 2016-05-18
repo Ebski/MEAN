13fabb5c758432b85e7222b3ba70704887f1041f
Ebski <ebbe_vig@hotmail.com>
2016-05-18 22:43:53 +0200

    Period3b

---
+++ Period3b/RestAPI/server.js
@@ -0,0 +1,66 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var Contact = require("./model/contact");
+
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
+
+//CRUD GET request
+app.get('/contactlist', function (req, res) {
+    console.log('I received a GET request');
+
+    Contact.find(function (err, docs) {
+        res.json(docs);
+    });
+
+});
+
+//CRUD POST request
+app.post('/contactlist', function (req, res) {
+    console.log('I received a POST request');
+    console.log(req.body);
+
+    var newContact = new Contact({
+        name: req.body.name,
+        email: req.body.email,
+        number: req.body.number
+    })
+
+    newContact.save(function (err, doc) {
+        res.json(doc);
+    });
+});
+
+//CRUD DELETE request
+app.delete('/contactlist/:id', function (req, res) {
+    var id = req.params.id;
+    console.log(id);
+
+    Contact.remove({_id: id}, function (err, doc) {
+        res.json(doc);
+    })
+});
+
+app.get('/contactlist/:id', function (req, res) {
+    var id = req.params.id;
+    console.log(id);
+
+    Contact.findOne({_id: id}, function (err, doc) {
+        res.json(doc);
+    })
+});
+
+app.put('/contactlist/:id', function (req, res) {
+    var id = req.params.id;
+    console.log(req.body.name);
+
+    Contact.findOneAndUpdate({_id: id},
+        {$set: {name: req.body.name, email: req.body.email, number: req.body.number}}, function (err, doc) {
+            res.json(doc);
+        })
+
+});
+
+app.listen(3000);
+console.log('Server running on localhost:3000')
