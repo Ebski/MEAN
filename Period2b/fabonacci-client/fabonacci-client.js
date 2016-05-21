d3ff42f1a17e2606491b144bbe32311130c7fabb
Ebski <ebbe_vig@hotmail.com>
2016-05-22 01:48:55 +0200

    added user defined module to the Period2 folder

---
+++ Period2b/fabonacci-client/fabonacci-client.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Ebbe on 22-05-2016.
+ */
+var fab = require('fabonacci');
+var num = fab.fabonacci(1,1);
+console.log(num);
---
+++ Period2b/fabonacci/fabonacci.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Ebbe on 22-05-2016.
+ */
+function fabonacci(num1,num2){
+    return num1+num2;
+};
+
+exports.fabonacci = fabonacci;
